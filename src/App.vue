45fc819330da69dd78821a2e5491875640ef5798
Avalon <littlecontrol2019@gmail.com>
2019-12-03 16:57:14 +0800

    底部导航栏

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <van-swipe :autoplay="3000">
+      <van-swipe-item v-for="(image, index) in images" :key="index">
+        <img v-lazy="image" width="375" height="200" />
+      </van-swipe-item>
+    </van-swipe>
+    <FooterGuide />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Lazyload } from "vant";
+import { Swipe, SwipeItem } from "vant";
+
+Vue.use(Lazyload);
+Vue.use(Swipe).use(SwipeItem);
+
+import FooterGuide from "./views/FooterGuide";
+export default {
+  data() {
+    return {
+      images: ["/img/app/2.jpg", "/img/app/3.jpg"]
+    };
+  },
+  components: {
+    FooterGuide
+  }
+};
+</script>
+
+<style>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
---
+++ src/views/FooterGuide.vue
@@ -0,0 +1,51 @@
+<template>
+  <van-tabbar v-model="active">
+    <van-tabbar-item>
+      <span>曲库</span>
+      <img
+        slot="icon"
+        slot-scope="props"
+        :src="props.active ? warehouse.active : warehouse.inactive"
+      />
+    </van-tabbar-item>
+    <van-tabbar-item>
+      <span>正在播放</span>
+      <img slot="icon" slot-scope="props" :src="props.active ? player.active : player.inactive" />
+    </van-tabbar-item>
+    <van-tabbar-item>
+      <span>我的</span>
+      <img slot="icon" slot-scope="props" :src="props.active ? mine.active : mine.inactive" />
+    </van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<script>
+import Vue from "vue";
+import { Tabbar, TabbarItem } from "vant";
+import { Icon } from "vant";
+
+Vue.use(Icon);
+Vue.use(Tabbar).use(TabbarItem);
+export default {
+  data() {
+    return {
+      active: 0,
+      warehouse: {
+        active: "/img/footer_guide/warehouse_active.png",
+        inactive: "/img/footer_guide/warehouse_inactive.png"
+      },
+      player: {
+        active: "/img/footer_guide/player_active.png",
+        inactive: "/img/footer_guide/player_inactive.png"
+      },
+      mine: {
+        active: "/img/footer_guide/mine_active.png",
+        inactive: "/img/footer_guide/mine_inactive.png"
+      }
+    };
+  }
+};
+</script>
+ 
+<style>
+</style>
