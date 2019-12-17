fc18101576796b6805d7ead8e22f89a754f5d359
Avalon <littlecontrol2019@gmail.com>
2019-12-17 20:41:30 +0800

    Mine组件的基本排版完成

--- src/views/Mine.vue
+++ src/views/Mine.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="all_border">
-    <div class="my_logo"></div>
     <van-divider :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }">
-      已累计呆在这里
+      你已已经呆在这里
       <van-count-down millisecond format="HH:mm:ss:SS" />时间啦
     </van-divider>
-    <div class="circles">
-      <van-circle v-model="currentRate" :color="gradientColor" text="渐变色" />
-      <van-circle v-model="currentRate" :color="gradientColor" text="渐变色" />
-      <van-circle v-model="currentRate" :color="gradientColor" text="渐变色" />
+    <div class="my_logo">
+      <div class="logo_img">
+        <img src="/img/mine/Emiya.jpg" />
+      </div>
     </div>
-    <van-divider :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }">文字</van-divider>
+    <van-divider :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }">我的标签</van-divider>
     <div class="tags">
-      <van-tag plain>标签</van-tag>
-      <van-tag plain type="primary">标签</van-tag>
-      <van-tag plain type="success">标签</van-tag>
-      <van-tag plain type="danger">标签</van-tag>
-      <van-tag plain type="warning">标签</van-tag>
+      <van-tag plain>李荣浩</van-tag>
+      <van-tag plain type="primary">民谣</van-tag>
+      <van-tag plain type="success">轻音乐</van-tag>
+      <van-tag plain type="danger">节奏控</van-tag>
+      <van-tag plain type="warning">钢琴</van-tag>
     </div>
-    <van-divider :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }">文字</van-divider>
+    <van-divider :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }">我的歌单</van-divider>
     <div class="song_sheet">
-      <van-skeleton title avatar :row="3" />
-      <van-skeleton title avatar :row="3" />
-      <van-skeleton title avatar :row="3" />
+      <div class="sheet">
+        <div class="cover">
+          <img src="/img/mine/sheet1.jpg" />
+        </div>
+        <div class="description">
+          <div class="title">Trouble I'm In</div>
+          <div class="commit">你眼中的世界</div>
+          <div class="time">创建于2019-11-23</div>
+        </div>
+        <div class="nav_arrow">
+          <img src="/img/mine/arrow.png" />
+        </div>
+      </div>
+      <div class="sheet">
+        <div class="cover">
+          <img src="/img/mine/sheet1.jpg" />
+        </div>
+        <div class="description">
+          <div class="title">Trouble I'm In</div>
+          <div class="commit">你眼中的世界</div>
+          <div class="time">创建于2019-11-23</div>
+        </div>
+        <div class="nav_arrow">
+          <img src="/img/mine/arrow.png" />
+        </div>
+      </div>
+      <div class="sheet">
+        <div class="cover">
+          <img src="/img/mine/sheet1.jpg" />
+        </div>
+        <div class="description">
+          <div class="title">Trouble I'm In</div>
+          <div class="commit">你眼中的世界</div>
+          <div class="time">创建于2019-11-23</div>
+        </div>
+        <div class="nav_arrow">
+          <img src="/img/mine/arrow.png" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -30,7 +65,6 @@
 <script>
 import Vue from "vue";
 import { Skeleton } from "vant";
-import { Circle } from "vant";
 import { Divider } from "vant";
 import { CountDown } from "vant";
 import { Tag } from "vant";
@@ -38,17 +72,10 @@ import { Tag } from "vant";
 Vue.use(Tag);
 Vue.use(CountDown);
 Vue.use(Divider);
-Vue.use(Circle);
 Vue.use(Skeleton);
 export default {
   data() {
-    return {
-      currentRate: 0,
-      gradientColor: {
-        "0%": "#3fecff",
-        "100%": "#6149f6"
-      }
-    };
+    return {};
   }
 };
 </script>
@@ -57,9 +84,18 @@ export default {
 #all_border {
   box-sizing: content-box;
   padding: 20px;
-  .circles {
-    display: flex;
-    justify-content: space-around;
+  .my_logo {
+    text-align: center;
+    .logo_img {
+      width: 50px;
+      height: 50px;
+      margin: auto;
+      border-radius: 50% 50%;
+      overflow: hidden;
+      img {
+        width: 50px;
+      }
+    }
   }
   .tags {
     span {
@@ -67,6 +103,41 @@ export default {
     }
     display: flex;
     justify-content: space-around;
+  }
+  .song_sheet {
+    .sheet {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      .cover {
+        width: 100px;
+        height: 100px;
+        border-radius: 50% 50%;
+        overflow: hidden;
+        img {
+          width: 100px;
+        }
+      }
+      .description {
+        flex-basis: 50%;
+        display: flex;
+        align-content: space-around;
+        flex-wrap: wrap;
+        div {
+          width: 100%;
+        }
+      }
+      .nav_arrow {
+        img {
+          width: 20px;
+          position: relative;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        }
+      }
+      margin-bottom: 10px;
+    }
   }
 }
 </style>
